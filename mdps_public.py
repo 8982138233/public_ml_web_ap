d0ff67d4bd5a8b60fb419856834aead37fcc8e34
Rudra sharma <131862591+8982138233@users.noreply.github.com>
2024-03-18 22:02:03 +0530

    Update mdps_public.py

--- mdps_public.py
+++ mdps_public.py
@@ -34,10 +34,10 @@ with st.sidebar:
     
 
 # Diabetes Prediction Page
-    if (selected == 'Diabetes Prediction'):
+if (selected == 'Diabetes Prediction'):
     
     # page title
-        st.title('Diabetes Prediction using ML')
+    st.title('Diabetes Prediction using ML')
     
     
     # getting the input data from the user
@@ -82,6 +82,57 @@ with st.sidebar:
           diab_diagnosis = 'The person is not diabetic'
         
     st.success(diab_diagnosis)
+
+
+
+
+# Heart Disease Prediction Page
+if (selected == 'Heart Disease Prediction'):
+    
+    # page title
+    st.title('Heart Disease Prediction using ML')
+    
+    col1, col2, col3 = st.columns(3)
+    
+    with col1:
+        age = st.text_input('Age')
+        
+    with col2:
+        sex = st.text_input('Sex')
+        
+    with col3:
+        cp = st.text_input('Chest Pain types')
+        
+    with col1:
+        trestbps = st.text_input('Resting Blood Pressure')
+        
+    with col2:
+        chol = st.text_input('Serum Cholestoral in mg/dl')
+        
+    with col3:
+        fbs = st.text_input('Fasting Blood Sugar > 120 mg/dl')
+        
+    with col1:
+        restecg = st.text_input('Resting Electrocardiographic results')
+        
+    with col2:
+        thalach = st.text_input('Maximum Heart Rate achieved')
+        
+    with col3:
+        exang = st.text_input('Exercise Induced Angina')
+        
+    with col1:
+        oldpeak = st.text_input('ST depression induced by exercise')
+        
+    with col2:
+        slope = st.text_input('Slope of the peak exercise ST segment')
+        
+    with col3:
+        ca = st.text_input('Major vessels colored by flourosopy')
+        
+    with col1:
+        thal = st.text_input('thal: 0 = normal; 1 = fixed defect; 2 = reversable defect')
+        
 
 
 
