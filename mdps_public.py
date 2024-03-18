fd480938e8ba02e50834e8e392fb3b245a0d754f
Rudra sharma <131862591+8982138233@users.noreply.github.com>
2024-03-19 00:14:15 +0530

    Update mdps_public.py

--- mdps_public.py
+++ mdps_public.py
@@ -154,10 +154,8 @@ if (selected == 'Heart Disease Prediction'):
 
 # Parkinson's Prediction Page
 if (selected == " Heart Disease And Diabetes Specialist Doctor Suggestion"):
-
-    st.title('Heart Disease Prediction using ML')
     
-    st.title(" Heart Disease And Diabetes Speacialist Doctor Contacts ")
+    st.title("Heart Disease And Diabetes Speacialist Doctor Contacts")
     
 # Create a button, that when clicked, shows a text
 if(st.button("Dr.Navneet Agarwal")):
