a01c90f97697f153447eb5f0fad7991d960ce462
Rudra sharma <131862591+8982138233@users.noreply.github.com>
2024-03-18 23:15:08 +0530

    Update mdps_public.py

--- mdps_public.py
+++ mdps_public.py
@@ -212,7 +212,7 @@ if (selected == "Specialist Doctor Suggestion"):
         st.button("Click me for no reason")
  
 # Create a button, that when clicked, shows a text
-    if(st.button("About")):
+if(st.button("About")):
         st.text("Welcome To GeeksForGeeks!!!")
     
      # page title
