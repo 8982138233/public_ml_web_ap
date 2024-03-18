5d34b098b86bceb1e0c37ce97e20958b73178835
Rudra sharma <131862591+8982138233@users.noreply.github.com>
2024-03-18 20:51:34 +0530

    Update mdps_public.py

--- mdps_public.py
+++ mdps_public.py
@@ -159,8 +159,11 @@ if (selected == "Specialist Doctor Suggestion"):
     
      # page title
     st.title("Diabetes Speacialist Doctor Contacts ")
-    st.title("Dr Navneet Agarwal - 18605001066 ")
-    st.title("Dr Pankaj Gupta - 9827869595")
-    st.title("Dr Mukesh Gupta - 6269841913")
-    st.title("Dr R.K.Singhal - 6269841913")
-  
+    st.title("Dr.Navneet Agarwal - 18605001066 ")
+    st.title("Dr.Pankaj Gupta - 9827869595")
+    st.title("Dr.R.K Singhal - 6269841913")
+    
+    st.title("Diabetes Speacialist Doctor Contacts")
+    st.title("Dr.Dalmia - 9425752462")
+    st.title("Dr.Ram Rawat - 07512436093")
+    st.title("Dr.Dushyant Deo - 7700271131")
