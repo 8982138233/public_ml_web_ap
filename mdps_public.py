1ef0732c771770e4ff549ffeff6b4f145ed0a341
Rudra sharma <131862591+8982138233@users.noreply.github.com>
2024-03-18 20:26:04 +0530

    Update mdps_public.py

--- mdps_public.py
+++ mdps_public.py
@@ -229,28 +229,4 @@ if (selected == "Specialist Doctor Suggestion"):
         PPE = st.text_input('PPE')
         
     
-    
-    # code for Prediction
-    parkinsons_diagnosis = ''
-    
-    # creating a button for Prediction    
-    if st.button("Parkinson's Test Result"):
-        parkinsons_prediction = parkinsons_model.predict([[fo, fhi, flo, Jitter_percent, Jitter_Abs, RAP, PPQ,DDP,Shimmer,Shimmer_dB,APQ3,APQ5,APQ,DDA,NHR,HNR,RPDE,DFA,spread1,spread2,D2,PPE]])                          
-        
-        if (parkinsons_prediction[0] == 1):
-          parkinsons_diagnosis = "The person has Parkinson's disease"
-        else:
-          parkinsons_diagnosis = "The person does not have Parkinson's disease"
-        
-    st.success(parkinsons_diagnosis)
-
-
-
-
-
-
-
-
-
-
-
+   
