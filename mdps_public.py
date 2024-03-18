79c9edc761ab22848a47050e264327e0949071c2
Rudra sharma <131862591+8982138233@users.noreply.github.com>
2024-03-19 00:01:01 +0530

    Update mdps_public.py

--- mdps_public.py
+++ mdps_public.py
@@ -209,4 +209,25 @@ if (selected == "Heart Disease And Diabetes Specialist Doctor Suggestion"):
     # page title
     st.title("Diabetes Speacialist Doctor Contacts ")
     
+# Create a button, that when clicked, shows a text
+if(st.button("Dr.Navneet Agarwal")):
+    st.text("Mobile number - 1860500106 .... Address - Laltitpur Colony Gwalior")
+    
+if(st.button("Dr.Pankaj Gupta")):
+    st.text("Mobile Number - 9827869595 .... Address - Tulsi Vihar Colony Gwalior")
+
+if(st.button("Dr.R.K Singhal")):       
+    st.text(" Mobile Number - 6269841913 .... Address - Janakganj Gwalior")
+
+if(st.button("Dr.Dalmia")):     
+    st.text("Mobile Number - 9425752462 ..... Address - Gole Ke Mandir Gwalior")
+
+if(st.button("Dr.Ram Rawat")):  
+    st.text("Mobile Number - 07512436093 .... Address - Kampoo Bus stand Gwalior")
+
+if(st.button("Dr.Dushyant Deo")):     
+    st.text("Mobile Number - 7700271131 .... Address jhinshi Road no.3 gwalior  ")
+
+   
+    
 
