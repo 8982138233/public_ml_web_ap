6031932edb2f789211b6590d313ccc0aab54f020
Rudra sharma <131862591+8982138233@users.noreply.github.com>
2024-03-18 21:54:39 +0530

    Update mdps_public.py

--- mdps_public.py
+++ mdps_public.py
@@ -33,16 +33,7 @@ with st.sidebar:
                           default_index=0)
     
 
-with st.chat_message("user"):
-    st.write("Hello")
-    st.line_chart(np.random.randn(30, 3))
 
-# Display a chat input widget at the bottom of the app.
-    st.chat_input("Say something")
-
-# Display a chat input widget inline.
-with st.container():
-    st.chat_input("Say something")
 
 
 
@@ -180,4 +171,15 @@ if (selected == "Specialist Doctor Suggestion"):
     st.text("Dr.Dalmia - 9425752462")
     st.text("Dr.Ram Rawat - 07512436093")
     st.text("Dr.Dushyant Deo - 7700271131")
+
+    with st.chat_message("user"):
+        st.write("Hello")
+         st.line_chart(np.random.randn(30, 3))
+
+# Display a chat input widget at the bottom of the app.
+        st.chat_input("Say something")
+
+# Display a chat input widget inline.
+    with st.container():
+        st.chat_input("Say something")
     
