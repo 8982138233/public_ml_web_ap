27d6d48bb14c234ba3816cf0f1b546c921005a2f
Rudra sharma <131862591+8982138233@users.noreply.github.com>
2024-03-19 08:56:06 +0530

    Update mdps_public.py

--- mdps_public.py
+++ mdps_public.py
@@ -77,7 +77,7 @@ if (selected == 'Diabetes Prediction'):
         
         if (diab_prediction[0] == 1):
           diab_diagnosis = 'The Person Is Diabetic'
-         st.markdown(":red[You have take prcaution]")
+        st.markdown(":red[You have take prcaution]")
         else:
           diab_diagnosis = 'The Person Is Not Diabetic'
         
